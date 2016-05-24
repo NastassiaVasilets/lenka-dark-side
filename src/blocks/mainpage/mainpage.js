492950c7c30194e163db8c24924350bf5f54ed7f
NastassiaVasilets <Nastena_vg@mail.ru>
2016-05-25 00:06:25 +0300

    good work of pop-ups (full-session in index)

--- src/blocks/mainpage/mainpage.js
+++ src/blocks/mainpage/mainpage.js
@@ -1,15 +1,13 @@
-$(function () {
-	//script for popups
-	$('button_icon-look').click(function () {
-		$('div.'+$(this).attr("class", "popup")).fadeIn(500);
-		$("body").append("<div id='overlay'></div>");
-		$('#overlay').show().css({'filter' : 'alpha(opacity=80)'});
-		return false;				
-	});	
-	$('a.close').click(function () {
-		$(this).parent().fadeOut(100);
-		$('#overlay').remove('#overlay');
-		return false;
+$(document).ready(function(){
+	$('.popup .close_window, .overlay').click(function (){
+		$(this).parents('div.session').children('.popup').css({'opacity':'0', 'visibility':'hidden'});
+		$(this).parents('div.session').children('.overlay').css({'opacity':'0', 'visibility':'hidden'});
 	});
-});	
+	$('.button_icon-look').click(function (e){
+		console.log($(this).parents('div.session').children('.popup'));
+		$(this).parents('div.session').children('.popup').css({'opacity':'1', 'visibility':'visible'});
+		$(this).parents('div.session').children('.overlay').css({'opacity':'1', 'visibility':'visible'});
+		e.preventDefault();
+	});
+});
 
