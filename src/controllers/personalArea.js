15065a766c93b796778227cdba6a084b6347fddb
NastassiaVasilets <Nastena_vg@mail.ru>
2016-05-25 19:13:57 +0300

    count debts but not view them

--- src/controllers/personalArea.js
+++ src/controllers/personalArea.js
@@ -11,6 +11,43 @@ module.exports = function personalArea(req, res) {
 				orders.push(allOrders[i]);
 			}
 		}
-		res.render('personalArea', {orders: orders,user:req.user})
+		//доделать вывод в историю тех заказов на которые я была подписана и сортировку их по времени
+		var my = [];
+		var me = [];
+		//высчитываем сколько мне должны
+		for (var i = 0; i < allOrders.length; i++) {
+			if (allOrders[i].owner.id == req.user.id) {
+				for (var j = 0; j < allOrders[i].subscriber.length; j++) {
+					if (allOrders[i].subscriber[j].paid == false) {
+						var dept = {};
+						dept.person = allOrders[i].subscriber[j].person.name;
+						var sum = 0;
+						for (var k = 0; k < allOrders[i].subscriber[j].dishes.length; k++) {
+							sum += allOrders[i].subscriber[j].dishes[k].price;
+						}
+						dept.sum = sum;
+						me.push(dept);
+					}
+				}
+			}
+		}
+		//высчитываем сколько я должна
+		for (var i = 0; i < allOrders.length; i++) {
+			for (var j = 0; j < allOrders[i].subscriber.length; j++) {
+				if (allOrders[i].subscriber[j].person.id == req.user.id) {
+					var dept = {};
+					dept.person = allOrders[i].owner.name;
+					var sum = 0;
+					for (var k = 0; k < allOrders[i].subscriber[j].dishes.length; k++) {
+						sum += allOrders[i].subscriber[j].dishes[k].price;
+					}
+					dept.sum = sum;
+					my.push(dept);
+				}
+			}
+		}
+		console.log('me',me);
+		console.log('my',my);
+		res.render('personalArea', {orders: orders,user:req.user, my: my, me: me});
 	});
 }
